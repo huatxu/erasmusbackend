ae1625b7fac0fb4e01c5d801d07cbb19f4265bfa
andres.sole <andres.sole@meaningfuldata.eu>
2021-08-18 14:10:42 +0200

    TYPO

--- erasmusbackend/settings.py
+++ erasmusbackend/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',

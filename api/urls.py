33b5dab0a4ce7e296a7dbc58b039909f7bf804fe
andres.sole <andres.sole@meaningfuldata.eu>
2021-08-18 13:57:30 +0200

    csfr

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from api import views
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 urlpatterns = [
-    path('comida/', views.ComidaList.as_view()),
-    path('cerveza/', views.CervezaList.as_view()),
+    path('comida/', csrf_exempt(views.ComidaList.as_view())),
+    path('cerveza/', csrf_exempt(views.CervezaList.as_view())),
 ]

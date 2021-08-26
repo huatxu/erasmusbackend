ab81b4aeb2177cf1f0d29a4d63e7f69decb9bd37
andres.sole <andres.sole@meaningfuldata.eu>
2021-08-26 22:03:49 +0200

    improvement back-end: better usability

--- api/admin.py
+++ api/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 from api.models import Cerveza, Comida, Titulo
 
 class CervezaAdmin(admin.ModelAdmin):
-    list_display = ('id','nombre')
+    list_display = ('id','nombre', 'tipo', 'disponible', 'barril', 'precio', 'precio_2', 'precio_3')
     search_fields = ('nombre',)
+    list_filter = ('tipo','disponible','barril')
+    list_editable = ('disponible',)
+
 class ComidaAdmin(admin.ModelAdmin):
-    list_display = ('id','nombre')
+    list_display = ('id','nombre', 'tipo', 'disponible', 'precio', 'precio_2')
     search_fields = ('nombre',)
+    list_filter = ('tipo','disponible',)
+    list_editable = ('disponible', )
 
 class TituloAdmin(admin.ModelAdmin):
     list_display = ('titulo_1',)
